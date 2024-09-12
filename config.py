304ba9c428a03ac47d79453525192ef2b47c044a
shruti148 <79242202+shruti148@users.noreply.github.com>
2024-09-12 06:13:31 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,27 @@
+
+import os
+from   decouple import config
+
+class Config(object):
+
+    basedir    = os.path.abspath(os.path.dirname(__file__))
+
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+    # Security
+    SESSION_COOKIE_HTTPONLY  = True
+    REMEMBER_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_DURATION = 3600
+
+
+class DebugConfig(Config):
+    DEBUG = True
+
+# Load all possible configurations
+config_dict = {
+    'Production': ProductionConfig,
+    'Debug'     : DebugConfig
+}
